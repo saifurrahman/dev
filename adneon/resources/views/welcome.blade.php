477671f831f47abf5fb9b89bcf75379aa1223bb2
Saifur Rahman <saifur.rahman@glomindz.com>
2016-01-20 15:59:22 +0530

    adneonv2

---
+++ adneon/resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <meta charset="ISO-8859-1">
+        <meta property="og:title" content="DSMG monitoring module" />
+        <meta property="og:image" content="packages/images/logo.png" />
+          <title>DSMG monitoring module</title>
+
+        <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 76px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">AD neon</div>
+                <div>Rockland Media and Communication Private Limited</div>
+                <div class="row">
+                    <form action="" method="post">
+                      <input type="text" name="userid"></input>
+                      <input type="password" name="password"></input>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
--- nfr/views/user/reports.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-	<div class="row">
-		<h3>Reports</h3>
-	</div>
-</div>
